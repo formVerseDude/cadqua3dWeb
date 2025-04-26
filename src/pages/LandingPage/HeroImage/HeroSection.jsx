7efb1ee05e9670b04a4444fc30d4a3899f222176
Anmol-Slooze <anmol_k@slooze.xyz>
2025-04-26 22:00:46 +0530

    Update HeroSection text and styles for improved layout and responsiveness. Adjusted text formatting and added margin to words for better spacing. Enhanced button container with z-index for proper layering.

--- src/pages/LandingPage/HeroImage/HeroSection.jsx
+++ src/pages/LandingPage/HeroImage/HeroSection.jsx
@@ -14,7 +14,7 @@ export default function HeroSection() {
     }),
   };
 
-  const text = "Together Let’s Print the Future";
+  const text = "Together, Let’s Print the Future";
   const letters = text.split(" ");
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
@@ -32,11 +32,16 @@ export default function HeroSection() {
           animate={isInView ? "show" : "hidden"}
           variants={variants}
           viewport={{ once: true }}
-          className="flex justify-center text-[#938068] text-[60px] font-blacksword sm:text-4xl md:text-6xl"
+          className="flex justify-center text-[#938068] text-[20px] font-blacksword sm:text-4xl md:text-6xl"
         >
           {letters.map((word, i) => (
-            <motion.span key={`${word}-${i}`} variants={variants} custom={i}>
-              {word}{" "}
+            <motion.span
+              className="mr-[20px]"
+              key={`${word}-${i}`}
+              variants={variants}
+              custom={i}
+            >
+              {word}
             </motion.span>
           ))}
         </motion.h2>
@@ -84,7 +89,7 @@ export default function HeroSection() {
         />
       </div>
 
-      <div className="absolute bottom-14 w-full flex justify-center">
+      <div className="absolute bottom-14 w-full flex justify-center z-20">
         <button className="cursor-pointer group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-[#5E0C1C] hover:before:[box-shadow:_20px_20px_20px_30px] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-[#5E0C1C] relative bg-transparent h-16 w-64 border text-left p-3 text-[#938068] text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-[#5E0C1C] before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-[#938068] after:right-8 after:top-3 after:rounded-full after:blur-lg">
           Get Instant Quote
         </button>

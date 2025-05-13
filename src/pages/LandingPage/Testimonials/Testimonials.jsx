19cc7bb09ea9ed7590b610b11a31c92e79299220
Kanwar07 <anmolkanwar07@gmail.com>
2025-05-13 19:11:13 +0530

    Enhance Testimonials and WhatWeOffer components for improved functionality and responsiveness
    - Added useEffect hook in Testimonials component to automatically cycle through reviews every 5 seconds.
    - Updated transition duration and easing for smoother animations in the Testimonials component.
    - Improved responsiveness in WhatWeOffer component by adjusting class names for better layout on smaller screens.

--- src/pages/LandingPage/Testimonials/Testimonials.jsx
+++ src/pages/LandingPage/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import comma from "../../../assets/landingPage/comma.svg";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
@@ -52,6 +52,14 @@ export default function Testimonials() {
     );
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNextReview();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section id="testimonials">
       <motion.div
@@ -89,7 +97,7 @@ export default function Testimonials() {
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
-                  transition={{ duration: 0.5 }}
+                  transition={{ duration: 1, ease: "easeInOut" }}
                   className="flex flex-col gap-8 text-[#ffffff] "
                 >
                   <span className="text-[32px] font-semibold">
